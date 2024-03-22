c2c121a54f8fd3a755eb424aea4b6a921340fb04
Kingsley Amaeze <cmkslee@gmail.com>
2024-03-22 21:42:07 +0100

    added simple hello code

---
+++ src/main/java/com/propertly/propertlypoc/PropertlyPocApplication.java
@@ -0,0 +1,13 @@
+package com.propertly.propertlypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertlyPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertlyPocApplication.class, args);
+	}
+
+}
